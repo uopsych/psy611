2337d1cb171f00f311b7868fd6a823fdf8374535
Sara Weston <weston.sara@gmail.com>
2020-11-19 09:11:30 -0800

    update lecture 15, link lecture 16

---
+++ install_papaja.R
@@ -0,0 +1,5 @@
+# Install devtools package if necessary
+if(!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
+
+# Install the stable development verions from GitHub
+devtools::install_github("crsh/papaja")
